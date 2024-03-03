1a3e523a3abefdc6c05c82ed39bd773fdcc26f0b
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-03-03 21:31:35 +0530

    added search results

--- frontend/src/screens/Landing/Landing.jsx
+++ frontend/src/screens/Landing/Landing.jsx
@@ -1,8 +1,9 @@
+import React from "react";
 import {
   Box,
+  CircularProgress,
   Container,
   IconButton,
-  InputAdornment,
   Stack,
   TextField,
   Typography
@@ -10,48 +11,81 @@ import {
 import SearchIcon from "@mui/icons-material/Search";
 import ChatBot from "@/containers/ChatBot/ChatBot";
 import Section from "@/components/Section/Section";
-// import d from "@/data/d.json";
 import { useProducts } from "@/services/products.service.js";
+import { useBotSearch } from "@/services/bot.service";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 function Landing() {
   const products = useProducts();
 
+  const inputSchema = yup.object({
+    search: yup.string().required("Search is required")
+  });
+  const [searchResult, setSearchResult] = React.useState([]);
+
+  const chatBot = useBotSearch(handleBotResponse);
+
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(inputSchema)
+  });
+
+  function handleBotResponse(data) {
+    setSearchResult(JSON.parse(data.data.content));
+  }
+
+  const onSubmit = ({ search }) => {
+    chatBot.mutate({ role: "user", content: search });
+  };
+
   return (
     <>
       <Box className="background" sx={{ px: 10 }}>
-        <Stack alignItems="center" spacing={2} sx={{ py: 15 }}>
-          <Typography variant="h2" color="whitesmoke" textAlign="center">
-            “The only person you are destined to become is the person you decide
-            to be.”
-          </Typography>
-          <Typography
-            variant="h4"
-            color="whitesmoke"
-            textAlign="center"
-            sx={{ py: 2 }}
-            fontStyle="italic"
-          >
-            - Ralph Waldo Emerson
-          </Typography>
-          <TextField
-            variant="outlined"
-            sx={{
-              background: "white",
-              borderRadius: 1,
-              ml: 2,
-              maxWidth: "60%"
-            }}
-            fullWidth
-            placeholder="Search..."
-            InputProps={{
-              startAdornment: (
-                <IconButton type="submit">
-                  <SearchIcon />
-                </IconButton>
-              )
-            }}
-          />
-        </Stack>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack alignItems="center" spacing={2} sx={{ py: 15 }}>
+            <Typography variant="h2" color="whitesmoke" textAlign="center">
+              “The only person you are destined to become is the person you
+              decide to be.”
+            </Typography>
+            <Typography
+              variant="h4"
+              color="whitesmoke"
+              textAlign="center"
+              sx={{ py: 2 }}
+              fontStyle="italic"
+            >
+              - Ralph Waldo Emerson
+            </Typography>
+            <TextField
+              {...register("search")}
+              type="search"
+              variant="outlined"
+              sx={{
+                background: "white",
+                borderRadius: 1,
+                ml: 2,
+                maxWidth: "60%"
+              }}
+              onChange={(e) => {
+                if (e.target.value === "") {
+                  setSearchResult([]);
+                }
+              }}
+              fullWidth
+              placeholder="Search by AI..."
+              InputProps={{
+                endAdornment: chatBot.isPending ? (
+                  <CircularProgress />
+                ) : (
+                  <IconButton type="submit">
+                    <SearchIcon />
+                  </IconButton>
+                )
+              }}
+            />
+          </Stack>
+        </form>
       </Box>
       <Container maxWidth="xl" sx={{ py: 5 }}>
         <Section title={"Today’s Deals"} products={products} />
--- frontend/src/services/bot.service.js
+++ frontend/src/services/bot.service.js
@@ -2,6 +2,21 @@ import api from "@/helpers/api";
 import endpoints from "@/helpers/endpoints";
 import { useMutation } from "@tanstack/react-query";
 
+export const useBotSearch = (handleChatBotResponse) => {
+  return useMutation({
+    mutationKey: ["chatbot"],
+    mutationFn: (message) => {
+      return api({
+        method: "post",
+        url: endpoints.groq,
+        params: { chat_role: "search" },
+        data: { messages: [message] }
+      });
+    },
+    onSettled: handleChatBotResponse
+  });
+};
+
 export const useChatBot = (handleChatBotResponse) => {
   return useMutation({
     mutationKey: ["chatbot"],
